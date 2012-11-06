26dd8aaf3c0e48d38af7b597056d8a7aeb30cbe8
Jack Wu <jack.nan.wu@gmail.com>
2012-11-05 19:29:02 -0800

    Updated ipc_snd tests

--- c/snd_test.c
+++ c/snd_test.c
@@ -28,33 +28,65 @@ void sndtest_root(void)
 	kprintf("----------------------------------------------------------------------------\n");
 	pid = sysgetpid();
 
+
 #ifdef SEND_POSITIVE_TEST
+
 	child_pid[0] = syscreate(&sndtest_proc1, PROC_STACK);
 	child_pid[1] = syscreate(&sndtest_proc2, PROC_STACK);
 	child_pid[2] = syscreate(&sndtest_proc3, PROC_STACK);
 
+
 	syssleep(1000);
+
+	/* initial ipc_send to pass root pid to all child processes */
 	sprintf(buffer, "%d", n);
 	for(i=0 ; i<3 ; i++)
 		byte = syssend(child_pid[i], buffer, strlen(buffer));	
 
+
+	/*  
+	* @test: 	ipc_send_pos_1
+	*
+	* @desc:	the ipc_sender is able to unblock a blocked receiver from the middle of its blocked_receivers queue
+	*
+	* @outcome:	4 bytes of data is transferred from the sender to the receiver
+	*/
 	syssleep(1000);	
 	kprintf("[p%d]\t\t[blocked_send]\t\t\t[%d bytes]\t\t[p%d]\n", pid, strlen(buffer), child_pid[1]);
 	byte = syssend(child_pid[1], buffer, strlen(buffer));	
 	kprintf("[p%d]\t\t[unblocked_send]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, child_pid[1]);
 
+
+	/*  
+	* @test: 	ipc_send_pos_2
+	*
+	* @desc:	the ipc_sender is able to unblock a blocked receiver when the sender is transmissing less data than the receiver can handle
+	*
+	* @outcome:	3 bytes of data is transferred from the sender to the receiver
+	*/
 	syssleep(1000);	
-	n *= 1000;
+	n /= 10;	/* send only 3 bytes of data */
 	sprintf(buffer, "%d", n);
 	kprintf("[p%d]\t\t[blocked_send]\t\t\t[%d bytes]\t\t[p%d]\n", pid, strlen(buffer), child_pid[2]);
 	byte = syssend(child_pid[2], buffer, strlen(buffer));	
 	kprintf("[p%d]\t\t[unblocked_send]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, child_pid[2]);
 
+
 #elif defined SEND_NEGATIVE_TEST
+
 	syssleep(1000);	
+
+	/*  
+	* @test: 	ipc_send_neg_1
+	*
+	* @desc:	the ipc_sender is unablet to send an ipc message to itself
+	*
+	* @outcome:	-2 is returned from the syssend() system call
+	*/
 	kprintf("[p%d]\t\t[blocked_send]\t\t\t[%d bytes]\t\t[p%d]\n", pid, strlen(buffer), pid);
 	byte = syssend(pid, buffer, strlen(buffer));	
 	kprintf("[p%d]\t\t[unblocked_send]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, pid);
+
 #endif
 
 	/* eventual loop for send test root */
@@ -69,15 +101,19 @@ void sndtest_root(void)
 void sndtest_proc1(void)
 {
 #ifdef SEND_POSITIVE_TEST
-	unsigned int byte=4,n=0,pid;
-	unsigned int *ptr = &n;
-	unsigned char buffer[4];	
 
+	unsigned int byte=4,dst=0,pid;
+	unsigned int *ptr = &dst;
+	unsigned char buffer[10];	
 	pid = sysgetpid();
+
+	/* initial ipc_recv to get the root's pid */
 	byte = sysrecv(ptr, buffer, byte);
+
 	kprintf("[p%d]\t\t[blocked_receive]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, *ptr);
 	byte = sysrecv(ptr, buffer, byte);
 	kprintf("[p%d]\t\t[unblocked_receive]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, *ptr);
+
 #endif
 
 	for(;;);
@@ -91,15 +127,19 @@ void sndtest_proc1(void)
 void sndtest_proc2(void)
 {
 #ifdef SEND_POSITIVE_TEST
-	unsigned int byte=4,n=0,pid;
-	unsigned int *ptr = &n;
-	unsigned char buffer[4];	
 
+	unsigned int byte=4,dst=0,pid;
+	unsigned int *ptr = &dst;
+	unsigned char buffer[10];	
 	pid = sysgetpid();
+
+	/* initial ipc_recv to get the root's pid */
 	byte = sysrecv(ptr, buffer, byte);
+
 	kprintf("[p%d]\t\t[blocked_receive]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, *ptr);
 	byte = sysrecv(ptr, buffer, byte);
 	kprintf("[p%d]\t\t[unblocked_receive]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, *ptr);
+
 #endif
 
 	for(;;);
@@ -113,15 +153,19 @@ void sndtest_proc2(void)
 void sndtest_proc3(void)
 {
 #ifdef SEND_POSITIVE_TEST
-	unsigned int byte=4,n=0,pid;
-	unsigned int *ptr = &n;
-	unsigned char buffer[4];	
 
+	unsigned int byte=4,dst=0,pid;
+	unsigned int *ptr = &dst;
+	unsigned char buffer[10];	
 	pid = sysgetpid();
+
+	/* initial ipc_recv to get the root's pid */
 	byte = sysrecv(ptr, buffer, byte);
+
 	kprintf("[p%d]\t\t[blocked_receive]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, *ptr);
 	byte = sysrecv(ptr, buffer, byte);
 	kprintf("[p%d]\t\t[unblocked_receive]\t\t[%d bytes]\t\t[p%d]\n", pid, byte, *ptr);
+
 #endif
 
 	for(;;);
