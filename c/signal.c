526411b6087e197a1d6cb30a46105f1418d18a40
Jack Wu <jack.nan.wu@gmail.com>
2012-11-11 22:01:01 -0800

    Added sig_table puts function

--- c/signal.c
+++ c/signal.c
@@ -7,3 +7,20 @@
 #include <xeroskernel.h>
 
 /* Your code goes here */
+
+
+
+/*
+* puts_sig_table
+*
+* @desc:	output all signal handler address of user proc to console
+*/
+void puts_sig_table(pcb_t *p)
+{
+	int i;
+
+	kprintf("sig: ");
+	for(i=0 ; i<PROC_SZ ; i++)
+		kprintf("%d ", p->sig_table[i]);
+	kprintf("\n");
+}
