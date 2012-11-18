eb1a44160985d9606781e86c874a459adf6bd8fa
Jack Wu <jack.nan.wu@gmail.com>
2012-11-18 12:03:16 -0800

    Added device specific function prototypes and supplementary source files

--- c/kbd.c
+++ c/kbd.c
@@ -4,5 +4,47 @@
 * student id:   17254079
 */
 
+#include <xeroskernel.h>
+#include <scanToASCII.h>
 #include <kbd.h>
 
+
+int kbd_init(devsw_t* d)
+{
+
+	return 0;
+}
+
+int kbd_open(devsw_t* d)
+{
+
+
+	return 0;
+}
+
+int kbd_close(devsw_t* d)
+{
+
+
+	return 0;
+}
+
+int kbd_write(devsw_t* d, void* buf, int buflen)
+{
+
+	return 0;
+}
+
+int kbd_read(devsw_t* d, void* buf, int buflen)
+{
+
+
+	return 0;
+}
+
+int kbd_ioctl(devsw_t* d, unsigned long cmd)
+{
+
+
+	return 0;
+}
--- h/kbd.h
+++ h/kbd.h
@@ -5,5 +5,11 @@
 */
 
 
-#include <xeroskernel.h>
 
+extern int kbd_init(devsw_t* d);
+extern int kbd_open(devsw_t* d);
+extern int kbd_close(devsw_t* d);
+extern int kbd_write(devsw_t* d, void* buf, int buflen);
+extern int kbd_read(devsw_t* d, void* buf, int buflen);
+extern int kbd_ioctl(devsw_t* d, unsigned long cmd); 
+extern int kbd_iint();
